7c09530c079cefe00db57c85e6eb437973d54271
Yury Krylovich <yuricher@tut.by>
2023-10-10 20:05:29 +0200

    Api helper service implemented

---
+++ src/app/utils/apiRequestClass.ts
@@ -0,0 +1,94 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {
+  IBrandSearchResult,
+  IBrandsDatabase,
+  ICigarSearchResult,
+  ICigarsDatabase,
+  IRequestParameters,
+} from './types';
+import { IApiAssistantService } from './types';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ApiAssistantService implements IApiAssistantService {
+  constructor(private http: HttpClient) {}
+
+  private readonly API_HOST: string = 'cigars.p.rapidapi.com';
+  private readonly API_ENDPOINT: string = `https://${this.API_HOST}/`;
+  private readonly API_KEY: string =
+    'e22f14257fmsh4abd6503a233916p16b6e7jsn833b32e30f04';
+
+  private readonly searchKeysDictionary = {
+    paths: {
+      brands: 'brands',
+      cigars: 'cigars',
+    },
+    parameters: {
+      page: 'page',
+      search: 'search',
+      name: 'name',
+      brandId: 'brandId',
+      country: 'country',
+      filler: 'filler',
+      wrapper: 'wrapper',
+      color: 'color',
+      strength: 'strength',
+    },
+  };
+
+  private readonly options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': this.API_KEY,
+      'X-RapidAPI-Host': this.API_HOST,
+    },
+  };
+
+  getBrands(page: number, searchString?: string): Observable<IBrandsDatabase> {
+    const requestBody = `${this.searchKeysDictionary.paths.brands}?${this.searchKeysDictionary.parameters.page}=${page}`;
+    if (!searchString) {
+      return this.makeRequest(requestBody);
+    }
+    const searchQuery = `&${this.searchKeysDictionary.parameters.search}=${searchString}`;
+    return this.makeRequest(requestBody + searchQuery);
+  }
+
+  getBrandById(brandId: number): Observable<{ brand: IBrandSearchResult }> {
+    const requestBody = `${this.searchKeysDictionary.paths.brands}/${brandId}`;
+    return this.makeRequest(requestBody);
+  }
+
+  getCigars(
+    page: number,
+    requestDictionary?: IRequestParameters
+  ): Observable<ICigarsDatabase> {
+    const requestBody = `${this.searchKeysDictionary.paths.cigars}?${this.searchKeysDictionary.parameters.page}=${page}`;
+    if (!requestDictionary || !Object.keys(requestDictionary).length) {
+      return this.makeRequest(requestBody);
+    }
+
+    const queryStringArray = Object.keys(requestDictionary)
+      .filter((key) => key in this.searchKeysDictionary.parameters)
+      .map(
+        (key) =>
+          `${
+            this.searchKeysDictionary.parameters[
+              key as keyof IRequestParameters
+            ]
+          }=${requestDictionary[key as keyof IRequestParameters]}`
+      )
+      .join('&');
+
+    return this.makeRequest(`${requestBody}&${queryStringArray}`);
+  }
+
+  getCigarById(cigarId: number): Observable<{ cigar: ICigarSearchResult }> {
+    const requestBody = `${this.searchKeysDictionary.paths.cigars}/${cigarId}`;
+    return this.makeRequest(requestBody);
+  }
+
+  private makeRequest(requestBody: string): Observable<any> {
+    return this.http.get(this.API_ENDPOINT + requestBody, this.options);
+  }
+}
---
+++ src/app/utils/types.ts
@@ -0,0 +1,72 @@
+import { Observable } from 'rxjs';
+
+export interface IRequestParameters {
+  name?: string;
+  brandId?: number;
+  country?: string;
+  filler?: string;
+  wrapper?: string;
+  color?: CigarColor;
+  strength?: CigarStrength;
+}
+
+export interface IApiAssistantService {
+  getBrands(page: number, searchString?: string): Observable<IBrandsDatabase>;
+  getBrandById(brandId: number): Observable<{ brand: IBrandSearchResult }>;
+  getCigars(
+    page: number,
+    requestDictionary?: IRequestParameters
+  ): Observable<ICigarsDatabase>;
+  getCigarById(cigarId: number): Observable<{ cigar: ICigarSearchResult }>;
+}
+
+export interface IBrandsDatabase extends ICommonSearchResult {
+  brands: IBrandSearchResult[];
+}
+
+export interface ICigarsDatabase extends ICommonSearchResult {
+  cigars: ICigarSearchResult[];
+}
+
+export interface IBrandSearchResult {
+  brandId: number;
+  name: string;
+}
+
+export interface ICigarSearchResult {
+  cigarId: number;
+  brandId: number;
+  name: string;
+  length: number;
+  ringGauge: number;
+  country: string;
+  filler: string;
+  wrapper: string;
+  color: CigarColor;
+  strength: CigarStrength;
+}
+
+export interface ICommonSearchResult {
+  page: number;
+  count: number;
+}
+
+export type CigarColor =
+  | 'Colorado Maduro'
+  | 'Colorado Claro'
+  | 'Colorado'
+  | 'Maduro'
+  | 'Rosado'
+  | 'Naturale'
+  | 'Claro'
+  | 'Oscuro'
+  | 'Double Claro'
+  | 'Multiple'
+  | 'Barber Pole';
+
+export type CigarStrength =
+  | 'Mild'
+  | 'Mild-Medium'
+  | 'Medium'
+  | 'Medium-Full'
+  | 'Full';

5e1605cf7aecfb6e1d23af315fb2baadf9ba52d2
Yury Krylovich <yuricher@tut.by>
2023-10-10 20:05:54 +0200

    Other changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { InputComponent } from './components/input/input.component';
+import { ApiAssistantService } from './utils/apiRequestClass';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MainComponent, InputComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [ApiAssistantService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

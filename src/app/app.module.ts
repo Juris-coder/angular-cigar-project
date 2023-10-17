a766f0ba056d70861d8eacbd95a902490820fe70
Yury Krylovich <yuricher@tut.by>
2023-10-17 21:20:57 +0200

    ngModule updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { InputComponent } from './components/input/input.component';
-import { ApiAssistantService } from './utils/apiRequestClass';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApiAssistantService } from './utils/apiAssistantService';
+import { HttpClientModule } from '@angular/common/http';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { QuestionnaireComponent } from './components/questionnaire/questionnaire.component';
+import { FormLinkingDirective } from './link-form.directive';
+import { metaReducers, reducers } from './state/reducers';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, InputComponent],
+  declarations: [
+    AppComponent,
+    MainComponent,
+    InputComponent,
+    QuestionnaireComponent,
+    FormLinkingDirective,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     NgxSkeletonLoaderModule,
-    StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot(),
     StoreModule.forRoot(reducers, {
-      metaReducers
+      metaReducers,
     }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],

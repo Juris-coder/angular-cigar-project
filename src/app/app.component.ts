621f7f62633ee276562271951578e9a6b976dd93
Yury Krylovich <yuricher@tut.by>
2023-10-19 21:27:20 +0200

    styling and layout changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectUrl } from './state/selectors/cigarStore.selector';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private store: Store) {}
   title = 'cigar-project';
+
+  route$: Observable<string> = this.store.select(selectUrl);
 }

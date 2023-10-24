8eb4aa5c60358462efa2b5bd51770707a00994d9
Yury Krylovich <yuricher@tut.by>
2023-10-24 21:48:06 +0200

    class renamed

--- src/app/utils/apiAssistantService.ts
+++ src/app/utils/apiAssistantService.ts
@@ -10,7 +10,9 @@ import {
 import { IApiAssistantService } from './types';
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class ApiAssistantService implements IApiAssistantService {
   constructor(private http: HttpClient) {}
 

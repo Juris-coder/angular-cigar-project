b36c17de691f4e1e2a1d6cda812a80de5a67e9cb
Yury Krylovich <yuricher@tut.by>
2023-10-25 22:19:28 +0200

    local storage saver added

---
+++ src/app/state/reducers/localStorage.reducer.ts
@@ -0,0 +1,23 @@
+import { ActionReducer, State } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
+import { IStoreState } from '.';
+
+export const localStorageSyncReducer = (
+  reducer: ActionReducer<IStoreState>
+): ActionReducer<IStoreState> => {
+  return localStorageSync({
+    keys: [
+      {
+        questionnaire: [
+          'dateOfBirth',
+          'name',
+          'email',
+          'country',
+          'color',
+          'strength',
+        ],
+      },
+    ],
+    rehydrate: true,
+  })(reducer);
+};

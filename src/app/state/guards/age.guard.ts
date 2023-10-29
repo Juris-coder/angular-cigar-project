74fec4fb1f3adde4755965b34b83b3cb9748c762
Yury Krylovich <yuricher@tut.by>
2023-10-29 23:56:42 +0100

    age guard added

---
+++ src/app/state/guards/age.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectQuestionnaireData } from '../selectors/cigarStore.selector';
+import { Observable, map, take } from 'rxjs';
+import { isAdult } from 'src/app/utils/ageValidator';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AgeGuard {
+  constructor(private store: Store, private router: Router) {}
+  canActivate(): Observable<boolean> {
+    return this.store.select(selectQuestionnaireData).pipe(
+      take(1),
+      map(({ dateOfBirth }) => {
+        const date = isAdult(dateOfBirth);
+        if (date === undefined) {
+          this.router.navigate(['/questionnaire']);
+          return false;
+        }
+
+        if (date) {
+          return true;
+        }
+
+        this.router.navigate(['/questionnaire/restricted']);
+        return false;
+      })
+    );
+  }
+}
